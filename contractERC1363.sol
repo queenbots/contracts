055758c7dc2c086d4684936548093abc2bed4e2c
queenbots <116808625+queenbots@users.noreply.github.com>
2022-11-21 18:23:56 +0530

    Create contractERC1363.sol

---
+++ contractERC1363.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.8.0;
+
+abstract contract ERC1363 is ERC20, IERC1363, ERC165 {
+    using Address for address;
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
+        return interfaceId == type(IERC1363).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    function transferAndCall(address recipient, uint256 amount) public virtual override returns (bool) {
+        return transferAndCall(recipient, amount, "");
+    }
+
+    function transferAndCall(
+        address recipient,
+        uint256 amount,
+        bytes memory data
+    ) public virtual override returns (bool) {
+        transfer(recipient, amount);
+        require(_checkAndCallTransfer(_msgSender(), recipient, amount, data), "ERC1363: _checkAndCallTransfer reverts");
+        return true;
+    }
+
+    function transferFromAndCall(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        return transferFromAndCall(sender, recipient, amount, "");
+    }
+
+
+    function transferFromAndCall(
+        address sender,
+        address recipient,
+        uint256 amount,
+        bytes memory data
+    ) public virtual override returns (bool) {
+        transferFrom(sender, recipient, amount);
+        require(_checkAndCallTransfer(sender, recipient, amount, data), "ERC1363: _checkAndCallTransfer reverts");
+        return true;
+    }
+
+    function approveAndCall(address spender, uint256 amount) public virtual override returns (bool) {
+        return approveAndCall(spender, amount, "");
+    }
+
+    function approveAndCall(
+        address spender,
+        uint256 amount,
+        bytes memory data
+    ) public virtual override returns (bool) {
+        approve(spender, amount);
+        require(_checkAndCallApprove(spender, amount, data), "ERC1363: _checkAndCallApprove reverts");
+        return true;
+    }
+
+    function _checkAndCallTransfer(
+        address sender,
+        address recipient,
+        uint256 amount,
+        bytes memory data
+    ) internal virtual returns (bool) {
+        if (!recipient.isContract()) {
+            return false;
+        }
+        bytes4 retval = IERC1363Receiver(recipient).onTransferReceived(_msgSender(), sender, amount, data);
+        return (retval == IERC1363Receiver(recipient).onTransferReceived.selector);
+    }
+
+    function _checkAndCallApprove(
+        address spender,
+        uint256 amount,
+        bytes memory data
+    ) internal virtual returns (bool) {
+        if (!spender.isContract()) {
+            return false;
+        }
+        bytes4 retval = IERC1363Spender(spender).onApprovalReceived(_msgSender(), amount, data);
+        return (retval == IERC1363Spender(spender).onApprovalReceived.selector);
+    }
+}
