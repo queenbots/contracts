70607ec219d7d40737a103323c4967887feb942c
queenbots <116808625+queenbots@users.noreply.github.com>
2022-11-21 18:07:05 +0530

    Create ReentrancyGuard.sol

---
+++ ReentrancyGuard.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0; 
+    contract ReentrancyGuard {
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+    uint256 private _status;
+    constructor()  {
+        _status = _NOT_ENTERED;
+    }
+    modifier nonReentrant() {
+        _nonReentrantBefore();
+        _;
+        _nonReentrantAfter();
+    }
+    function _nonReentrantBefore() private {
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+        _status = _ENTERED;
+    }
+    function _nonReentrantAfter() private {
+        _status = _NOT_ENTERED;
+    }
+}
